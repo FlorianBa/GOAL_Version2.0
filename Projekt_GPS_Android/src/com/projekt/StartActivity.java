34a1c7bd96bf653a7a16bbeb5cd02624a8c341c7
Florian <BatzFlo@gmx.de>
2014-06-16 21:19:27 +0200

    Einrückungen

--- Projekt_GPS_Android/src/com/projekt/StartActivity.java
+++ Projekt_GPS_Android/src/com/projekt/StartActivity.java
@@ -23,29 +23,29 @@ import android.util.Log;
 public class StartActivity extends Activity implements OnClickListener {
 	private static final String KEY_LISTPREF = "saving";
 
-    public WifiManager wifi;
+	public WifiManager wifi;
 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start);
-		
+
 		Button startButton = (Button)findViewById(R.id.button_start);
 		Button settingsButton = (Button)findViewById(R.id.button_settings);
 		startButton.setOnClickListener(this);
 		settingsButton.setOnClickListener(this);
 
-        try {
-            turnHotSpotOnOff(1);
-        }catch (Exception e){
+		try {
+			turnHotSpotOnOff(1);
+		}catch (Exception e){
 
-            Log.d("Error", "Can't instantiate access point");
-        }
+			Log.d("Error", "Can't instantiate access point");
+		}
 
 
 
-    }
+	}
 
 	@Override
 	public void onClick(View view) {
@@ -65,7 +65,7 @@ public class StartActivity extends Activity implements OnClickListener {
 		super.onStart();
 		ArrayList<StorageInfo> listStorage = (ArrayList<StorageInfo>) StorageUtils.getStorageList();
 		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
-		
+
 		// Test if "extern" in Settings was selected before SD-Card is removed  
 		int storageCounter = 0;
 		for(@SuppressWarnings("unused") StorageInfo info: listStorage){
@@ -79,58 +79,58 @@ public class StartActivity extends Activity implements OnClickListener {
 				editor.commit();
 			}
 		}
-		
+
 		String storedPreference = pref.getString(KEY_LISTPREF, "intern");
-		
+
 		if(storedPreference.compareTo("intern") == 0){
 			Report.setExtStorage(false);
 		}else if(storedPreference.compareTo("extern") == 0){
 			Report.setExtStorage(true);
 		}
 	}
-    public void turnHotSpotOnOff(int i) throws Exception {
-        wifi = (WifiManager) getSystemService(StartActivity.this.WIFI_SERVICE);
-        WifiConfiguration config = new WifiConfiguration();
-        config.SSID = "123";
+	public void turnHotSpotOnOff(int i) throws Exception {
+		wifi = (WifiManager) getSystemService(StartActivity.this.WIFI_SERVICE);
+		WifiConfiguration config = new WifiConfiguration();
+		config.SSID = "123";
 
-        Method[] wmMethods = wifi.getClass().getDeclaredMethods();
+		Method[] wmMethods = wifi.getClass().getDeclaredMethods();
 
-        switch (i) {
-            case 1:
-                wifi.setWifiEnabled(false);
+		switch (i) {
+		case 1:
+			wifi.setWifiEnabled(false);
 
-                for (Method method : wmMethods) {
-                    if (method.getName().equals("setWifiApEnabled")) {
-                        method.invoke(wifi, config, true);
-                    }
-                }
-                break;
+			for (Method method : wmMethods) {
+				if (method.getName().equals("setWifiApEnabled")) {
+					method.invoke(wifi, config, true);
+				}
+			}
+			break;
 
-            case 2:
+		case 2:
 
-                for (Method method : wmMethods) {
-                    if (method.getName().equals("setWifiApEnabled")) {
+			for (Method method : wmMethods) {
+				if (method.getName().equals("setWifiApEnabled")) {
 
-                        method.invoke(wifi, null, false);
+					method.invoke(wifi, null, false);
 
-                    }
-                }
-                break;
+				}
+			}
+			break;
 
-        }
+		}
 
-    }
+	}
 
-    protected void onDestroy(){
-    	try {
+	protected void onDestroy(){
+		try {
 			turnHotSpotOnOff(2);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	wifi.setWifiEnabled(true);
-    	super.onDestroy();
-    }
+		wifi.setWifiEnabled(true);
+		super.onDestroy();
+	}
 
 }
 
